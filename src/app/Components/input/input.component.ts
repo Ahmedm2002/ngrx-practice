73370630b8a9a8f3c050587f8e47577bbdb103f1
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2025-05-20 01:59:15 +0500

    Added the funtionalites of toggle complete & delete todo

--- src/app/Components/input/input.component.ts
+++ src/app/Components/input/input.component.ts
@@ -15,8 +15,11 @@ export class InputComponent {
   addNewTodo() {
     const randomId = Math.floor(Math.random() * 99999);
     const currentDate = new Date();
-    if (new Date(this.newDate) < currentDate || new Date(this.newDate) == currentDate) {
-      console.log('Date cannot be past or on the same day');
+    if (
+      new Date(this.newDate) < currentDate ||
+      new Date(this.newDate) == currentDate
+    ) {
+      alert('Date cannot be past or on the same day');
     } else {
       this.store.dispatch(
         addTodo({
--- src/app/Components/todos-list/todos-list.component.ts
+++ src/app/Components/todos-list/todos-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
-import { selectAllTodos, totalTodos } from '../../Store/todo.selector';
+import { selectAllTodos } from '../../Store/todo.selector';
+import { Todo } from '../../Models/todo.model';
+import { removeTodo, updateTodo } from '../../Store/todo.actions';
 @Component({
   selector: 'app-todos-list',
   imports: [CommonModule, AsyncPipe],
@@ -11,4 +13,13 @@ import { selectAllTodos, totalTodos } from '../../Store/todo.selector';
 export class TodosListComponent {
   private store = inject(Store);
   todos$ = this.store.select(selectAllTodos);
+  deleteTodo(todo : Todo){
+    this.store.dispatch(removeTodo({ todoId : todo.id}))
+  }
+  onToggle(event : any, todo : Todo){
+    this.store.dispatch(updateTodo({updatedTodo : {
+      ...todo,
+      isCompleted : event.target.checked
+    }}))
+  }
 }
